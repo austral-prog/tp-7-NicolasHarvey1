dcb9609125984e3eb3a996d8213ba9b092323475
NicolasHarvey1 <162722159+NicolasHarvey1@users.noreply.github.com>
2024-05-17 12:27:12 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,22 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+	index = 0
+	while index < len(lista):
+		if lista[index]:
+			word = lista[index]
+			lista[index] = f"{index}. {word}"
+			index += 1
+		else:
+			del lista[index]		
+	return lista
 
 
-def enumerate_backwards(list):
-    return list
+def enumerate_backwards(lista2):
+	index = 0
+	while index < len(lista2):
+		if lista2[index]:
+			word = lista2[index][::-1]
+			lista2[index] = f"{index}. {word}"
+			index += 1
+		else:
+			del lista2[index]		
+	return lista2
